af5fc9d5cb37c691c9b028286f68976ec0d76723
Arkadiy A.Davydov <arkadiy.a.davydov@gmail.com>
2020-11-26 17:29:17 +0300

    chore(project): [BWF-47] add layout dialect

---
+++ src/main/java/com/blockwit/bwf/config/AppConfig.java
@@ -0,0 +1,13 @@
+package com.blockwit.bwf.config;
+
+import nz.net.ultraq.thymeleaf.LayoutDialect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+	@Bean
+	public LayoutDialect layoutDialect() {
+		return new LayoutDialect();
+	}
+}
--- src/main/java/com/blockwit/bwf/controller/PostController.java
+++ src/main/java/com/blockwit/bwf/controller/PostController.java
@@ -1,10 +1,12 @@
 package com.blockwit.bwf.controller;
 
 import com.blockwit.bwf.form.NewPostForm;
-import com.blockwit.bwf.repository.PostRepository;
+import com.blockwit.bwf.model.User;
 import com.blockwit.bwf.service.AccountService;
+import com.blockwit.bwf.service.PostService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,25 +20,25 @@ import javax.validation.Valid;
 @Controller
 public class PostController {
 
-	private final AccountService accountService;
-	private final PostRepository postService;
+	private final PostService postService;
 
-	public PostController(AccountService accountService, PostRepository postService) {
-		this.accountService = accountService;
+	public PostController(PostService postService) {
 		this.postService = postService;
 	}
 
 	@PostMapping("/posts/new")
-	public ModelAndView createPost(@ModelAttribute("newPost") @Valid NewPostForm newPostForm, BindingResult bindingResult) {
+	public ModelAndView createPost(@ModelAttribute("newPost") @Valid NewPostForm newPostForm, Authentication authentication, BindingResult bindingResult) {
 		if (bindingResult.hasErrors()) {
-			return new ModelAndView("front/post-new", bindingResult.getModel(), HttpStatus.BAD_REQUEST);
+			return new ModelAndView("pages/post-new", bindingResult.getModel(), HttpStatus.BAD_REQUEST);
 		}
+		User user = (User)authentication.getPrincipal();
+		postService.create(newPostForm.getBody(), newPostForm.getTitle(), user.getAccount().getId());
 		return new ModelAndView("");
 	}
 
 	@GetMapping("/posts/new")
-	public ModelAndView createPost() {
-		return new ModelAndView("front/post-new");
+	public ModelAndView createPost( Authentication authentication) {
+		return new ModelAndView("pages/post-new");
 	}
 
 }
