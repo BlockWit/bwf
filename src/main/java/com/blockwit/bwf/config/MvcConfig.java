c3a36f36db8c15dc9fa25b11c7e4f1852c1b9ef0
Arkadiy A.Davydov <arkadiy.a.davydov@gmail.com>
2020-12-02 19:00:56 +0300

    chore(config): configure Pageable to start from 1

--- src/main/java/com/blockwit/bwf/config/MvcConfig.java
+++ src/main/java/com/blockwit/bwf/config/MvcConfig.java
@@ -2,8 +2,12 @@ package com.blockwit.bwf.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
@@ -12,6 +16,13 @@ public class MvcConfig implements WebMvcConfigurer {
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/app/login").setViewName("front/login");
 		registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+	}
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+		PageableHandlerMethodArgumentResolver resolver = new PageableHandlerMethodArgumentResolver();
+		resolver.setOneIndexedParameters(true);
+		argumentResolvers.add(resolver);
 	}
 
 }
