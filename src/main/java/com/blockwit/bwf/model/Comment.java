9da152eda5288076100ab129d48fc021696cea8b
Alexander Maslov <ritterod@gmail.com>
2020-12-20 19:25:06 +0300

    chore(project): [BWF-61] add comment model
    Signed-off-by: Maslov Alexander <ritterod@gmail.com>

--- src/main/java/com/blockwit/bwf/model/Comment.java
+++ src/main/java/com/blockwit/bwf/model/Comment.java
@@ -1,38 +1,37 @@
 package com.blockwit.bwf.model;
 
+
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
 import java.io.Serializable;
-import java.util.Set;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @Entity
-@Table(name = "posts")
-public class Post implements Serializable {
+@Table(name = "comment")
+public class Comment implements Serializable {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 
 	@Column(nullable = false, columnDefinition = "TEXT")
-	private String body;
+	private String content;
 
-	@Column(nullable = false)
-	private PostStatus status;
+	@Column
+	private LocalDateTime timestamp;
 
-	@Column(nullable = false)
-	private String title;
+	@Column
+	private Comment parent;
+
+	@ManyToOne
+	@JoinColumn(name = "post_id", nullable = false)
+	private Post target;
 
 	@ManyToOne
 	@JoinColumn(name = "owner_id")
 	private Account owner;
-
-	@OneToMany(mappedBy="target")
-	private Set<Comment> comments;
-
-	@Column
-	private Boolean is_comment_enabled;
 }
